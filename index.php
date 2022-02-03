193bee351fbf1c37d8169f5def3950efe2f245a6
Minal Madhur <73118982+Minal5@users.noreply.github.com>
2022-02-03 22:05:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
 <?php
-$insert =false;
-if(isset($_POST['name'])){
-    //Set connection variables
-    $server ="localhost";
-    $username ="root";
-    $password ="";
-    
-    //Create a database connection
-    $con =mysqli_connect($server,$username,$password);
-    
-    //Check for connection success
-    if(!$con){
-        die("connection to this database failed due to".mysqli_connect_error());
-    
-    }
-    //echo "Success connecting to db";
-    
-    //Collect post variables
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
-    $sql="INSERT INTO `art`.`contact` (`name`, `email`, `phone`, `message`) VALUES ('$name', '$email', '$phone', '$message');";
-    
 
-   //Execute the query
-    if($con->query($sql) == true){
-        // echo "Successfully inserted";
+session_start();
+require_once ('php/CreateDb1.php');
+require_once ('php/component1.php');
 
-        //flag for successful insertion
-        $insert=true;
+// create instance of Createdb class
+$database = new CreateDb("art", "product");
+
+if (isset($_POST['add'])){
+    /// print_r($_POST['product_id']);
+    if(isset($_SESSION['cart'])){
+
+        $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if(in_array($_POST['product_id'], $item_array_id)){
+            echo "<script>alert('Product is already added in the cart..!')</script>";
+            echo "<script>window.location = 'index1.php'</script>";
+        }else{
+
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id' => $_POST['product_id']
+            );
+
+            $_SESSION['cart'][$count] = $item_array;
+        }
+
+    }else{
+
+        $item_array = array(
+                'product_id' => $_POST['product_id']
+        );
+
+        // Create new session variable
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
     }
-    else{
-        echo "ERROR: $sql <br> $con->error";
-    }
-    
-    //Close the database connection
-    $con->close();
-    
 }
+
+
 ?>
 
 <!DOCTYPE html>
